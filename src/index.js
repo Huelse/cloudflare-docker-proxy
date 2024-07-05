014f06621316b96bf99e8a02245df8ae4c0627cf
huelse <huelse@qq.com>
2024-07-05 10:14:17 +0800

    private configs

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.image.topmaxz.com": dockerHub,
+  "quay.image.topmaxz.com": "https://quay.io",
+  "gcr.image.topmaxz.com": "https://gcr.io",
+  "k8s-gcr.image.topmaxz.com": "https://k8s.gcr.io",
+  "k8s.image.topmaxz.com": "https://registry.k8s.io",
+  "ghcr.image.topmaxz.com": "https://ghcr.io",
+  "cloudsmith.image.topmaxz.com": "https://docker.cloudsmith.io",
+  "ecr.image.topmaxz.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.image.topmaxz.com": dockerHub,
 };
 
 function routeByHosts(host) {
